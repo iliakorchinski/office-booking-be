d9c208c49488c061a8adc50b908ac394a6ef05a1
Ilia Kortchinski <kortchinskii@gmail.com>
2025-12-02 12:55:20 +0300

    feat: add user auntentication

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private auth: AuthService) {}
+  @Post('register')
+  register(@Body() dto: RegisterDto) {
+    return this.auth.register(dto.email, dto.password);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginDto) {
+    return this.auth.login(dto.email, dto.password);
+  }
+
+  @Post('refresh')
+  refresh(@Body() body: { userId: number; refreshToken: string }) {
+    return this.auth.refreshTokens(body.userId, body.refreshToken);
+  }
+}
